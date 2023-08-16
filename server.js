8713f5725b547bde9ab07de096d0acbb184cb930
RoelYoon <roel.yoon@ggq.gg>
2023-08-16 12:58:56 +0900

    Added first skeleton

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const path = require("path");
+
+const app = express();
+app.use(express.static(path.join(__dirname,"./public")));
+
+app.get("/",
+async (req,res)=>{
+    await res.sendFile(path.resolve(__dirname,"./public/index.html"))
+})
+
+app.all('*',(req,res)=>{
+    res.send("<h1>404 boiii</h1>").status(404);
+});
+
+app.listen(65000,()=>{console.log("Listening at 34.83.209.8 port 65000")});

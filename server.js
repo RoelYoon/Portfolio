4e8adef4eb10263be53f690a7defd50a56589cc5
RoelYoon <roel.yoon@ggq.gg>
2023-08-16 13:17:46 +0900

    Skeleton setup

--- server.js
+++ server.js
@@ -13,4 +13,4 @@ app.all('*',(req,res)=>{
     res.send("<h1>404 boiii</h1>").status(404);
 });
 
-app.listen(80, "10.138.0.2", ()=>{console.log("Listening at IP 34.83.209.8 port 80")});
+app.listen(65000, "10.138.0.2", ()=>{console.log("Listening at IP 34.83.209.8 port 65000")});

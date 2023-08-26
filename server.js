53126c0343f4154381033a4893d28762bb61a15e
RoelYoon <roel.yoon@ggq.gg>
2023-08-26 18:45:58 +0900

    Added Three.js paths

--- server.js
+++ server.js
@@ -3,12 +3,15 @@ const path = require("path");
 
 const app = express();
 app.use(express.static(path.join(__dirname,"./public")));
-
+app.use(express.static(path.join(__dirname,"./node_modules")));
 app.get("/",
 async (req,res)=>{
-    await res.sendFile(path.resolve(__dirname,"./public/index.html"))
+    await res.sendFile(path.resolve(__dirname,"./public/index.html"));
 })
-
+app.get("/three",
+async (req,res)=>{
+    await res.sendFile(path.resolve(__dirname,"./node_modules/three/build/three.module.js"));
+})
 app.all('*',(req,res)=>{
     res.send("<h1>404 boiii</h1>").status(404);
 });

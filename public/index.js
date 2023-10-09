cbd684f7eebccdee0641ea95c258f7f65fa15504
RoelYoon <roel.yoon@ggq.gg>
2023-10-09 17:03:13 +0900

    Added title mode

--- public/index.js
+++ public/index.js
@@ -33,7 +33,7 @@ GLTFloader.load( 'https://roelyoon.github.io/Portfolio/3DModels/portTitle.glb', 
 
 //light
 const pLight = new THREE.PointLight(0xffffff,50000);
-pLight.position.set(0,10,1); 
+pLight.position.set(0,10,-1); 
 const hLight = new THREE.HemisphereLight( 0xffffff, 0x080820, 50000);
 const amblight = new THREE.AmbientLight(0x404040,500);
 scene.add(pLight,amblight); 

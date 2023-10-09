77f4b70ffbc8c896ac3c595fb8842538ce1480f3
RoelYoon <roel.yoon@ggq.gg>
2023-10-09 13:36:57 +0900

    Added arrows

--- public/index.js
+++ public/index.js
@@ -67,8 +67,8 @@ function leftArrClick(){
 function rightArrClick(){
     console.log("right");
 }
-querySelector("#leftArrow").addEventListener("onclick",leftArrClick());
-querySelector("#rightArrow").addEventListener("onclick",rightArrClick());
+document.querySelector("#leftArrow").addEventListener("onclick",leftArrClick());
+document.querySelector("#rightArrow").addEventListener("onclick",rightArrClick());
 
 window.addEventListener('resize', ()=>{
     sizes.width = window.innerWidth;

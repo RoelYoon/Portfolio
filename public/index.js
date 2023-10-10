f88ef69a6ab2a940ef119df26daa153e6e0a1514
RoelYoon <roel.yoon@ggq.gg>
2023-10-10 23:59:22 +0900

    Added description

--- public/index.js
+++ public/index.js
@@ -220,9 +220,9 @@ window.addEventListener('resize', ()=>{
 })
   window.addEventListener("wheel", function(e) {
     if(!sceneYLock[curScene] && (targetCameraPos.y<50 && targetCameraPos.y>-50)){
-        targetCameraPos.y-=(Math.abs(e.deltaY)>7 ? (e.deltaY > 0)?7:-7 : e.deltaY)/10; 
-        targetOrbitPos.y-=(Math.abs(e.deltaY)>7 ? (e.deltaY > 0)?7:-7 : e.deltaY)/10;
-        lerpFrames=5;
+        targetCameraPos.y-=(Math.abs(e.deltaY)>5 ? (e.deltaY > 0)?5:-5 : e.deltaY)/10; 
+        targetOrbitPos.y-=(Math.abs(e.deltaY)>5 ? (e.deltaY > 0)?5:-5 : e.deltaY)/10;
+        lerpFrames=2;
     }
     // code to increment object.position.z 
   }, true);

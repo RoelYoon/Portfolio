60460b257ee1b8038362587d6c7a69c8c9e815eb
RoelYoon <roel.yoon@ggq.gg>
2023-10-11 02:47:46 +0900

    Added interaction

--- public/interaction.js
+++ public/interaction.js
@@ -2346,7 +2346,7 @@ class Ticker extends EventDispatcher {
  * @class
  * @extends InteractionManager
  */
-class Interaction extends InteractionManager {
+export class Interaction extends InteractionManager {
   /**
    * @param {WebGLRenderer} renderer - A reference to the current renderer
    * @param {Scene} scene - A reference to the current scene

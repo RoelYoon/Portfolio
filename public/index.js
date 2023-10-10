84d41c615f67afc95bfed0fc02b51807bcdf9036
RoelYoon <roel.yoon@ggq.gg>
2023-10-11 00:14:22 +0900

    Added description

--- public/index.js
+++ public/index.js
@@ -225,7 +225,7 @@ window.addEventListener('resize', ()=>{
         let d = (Math.abs(dY)>5 ? (dY > 0)?5:-5 : dY)/(isTouchPad?10:2); 
         targetCameraPos.y-=d; 
         targetOrbitPos.y-=d;
-        lerpFrames=5;
+        lerpFrames=3;
     }
     // code to increment object.position.z 
   }, true);

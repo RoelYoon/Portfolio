8b619a207a9bed143e5a8743602f730fe2a2d02f
RoelYoon <roel.yoon@ggq.gg>
2023-10-10 23:28:07 +0900

    Added description

--- public/index.js
+++ public/index.js
@@ -146,8 +146,8 @@ camera.position.z = 30;
 scene.add(camera); 
 
 const canvas  = document.querySelector(".webgl");
-canvas.style.width = sizes.width; 
-canvas.style.height = sizes.height; 
+canvas.style.width = Math.floor(sizes.width * window.devicePixelRatio); 
+canvas.style.height = Math.floor(sizes.height * window.devicePixelRatio); 
 canvas.width = Math.floor(sizes.width * window.devicePixelRatio);
 canvas.height = Math.floor(sizes.height * window.devicePixelRatio);
 /*

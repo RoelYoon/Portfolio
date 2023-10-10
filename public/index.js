f082b12c2d7eacd869ea2e068165752825d3c1d5
RoelYoon <roel.yoon@ggq.gg>
2023-10-11 00:08:25 +0900

    Added description

--- public/index.js
+++ public/index.js
@@ -221,7 +221,8 @@ window.addEventListener('resize', ()=>{
   window.addEventListener("wheel", function(e) {
     if(!sceneYLock[curScene]){
         let isTouchPad = e.wheelDeltaY ? e.wheelDeltaY === -3 * e.deltaY : e.deltaMode === 0
-        let d = isTouchPad?(Math.abs(e.deltaY)>5 ? (e.deltaY > 0)?5:-5 : e.deltaY)/10:e.wheelDeltaY; 
+        let dY = isTouchPad?e.deltaY : e.wheelDeltaY; 
+        let d = (Math.abs(dY)>5 ? (dY > 0)?5:-5 : dY)/10; 
         targetCameraPos.y-=d; 
         targetOrbitPos.y-=d;
         lerpFrames=5;

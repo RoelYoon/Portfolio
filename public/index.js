ff27c4437cbf9cea6412cdb93eef5d5f53c29057
RoelYoon <roel.yoon@ggq.gg>
2023-10-13 00:47:12 +0900

    Added challenge 3

--- public/index.js
+++ public/index.js
@@ -225,7 +225,7 @@ sceneYLock.push(false);
 id++;
 
 //programming challenge 3 title
-addSprite(568,111,1/20,id,0,0,0,'https://roelyoon.github.io/Portfolio/Challenges/Challenge 3/title.png');
+addSprite(568,111,1/30,id,0,0,0,'https://roelyoon.github.io/Portfolio/Challenges/Challenge 3/title.png');
 
 //description 
 addSprite(568,62,1/25,id,0,-4,0,'https://roelyoon.github.io/Portfolio/Challenges/Challenge 3/description.png');
@@ -237,13 +237,13 @@ addSprite(506,664,1/40,id,-6.1,-12,0,'https://roelyoon.github.io/Portfolio/Chall
 addSprite(446,148,1/50,id,5,-12,0,'https://roelyoon.github.io/Portfolio/Challenges/Challenge 3/explanation.png');
 
 //drawSpiral() sprite
-addSprite(362,692,1/40,id,-6.1,-12,0,'https://roelyoon.github.io/Portfolio/Challenges/Challenge 3/drawSpiral.png');
+addSprite(362,692,1/40,id,-6,-20,0,'https://roelyoon.github.io/Portfolio/Challenges/Challenge 3/drawSpiral.png');
 
 //exaplanation 2
 addSprite(458,624,1/50,id,5,-12,0,'https://roelyoon.github.io/Portfolio/Challenges/Challenge 3/explanation2.png');
 
 //drawLayer() sprite
-addSprite(568,419,1/40,id,-6.1,-12,0,'https://roelyoon.github.io/Portfolio/Challenges/Challenge 3/drawLayer.png');
+addSprite(568,419,1/40,id,-6.1,-30,0,'https://roelyoon.github.io/Portfolio/Challenges/Challenge 3/drawLayer.png');
 
 //exaplanation 3
 addSprite(448,322,1/50,id,5,-12,0,'https://roelyoon.github.io/Portfolio/Challenges/Challenge 3/explanation3.png');

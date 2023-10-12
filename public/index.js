979ba37bddbe335fe00bde0630bf7f750216b935
RoelYoon <roel.yoon@ggq.gg>
2023-10-12 23:36:50 +0900

    Added challenge 2

--- public/index.js
+++ public/index.js
@@ -213,10 +213,10 @@ addSprite(422,404,1/40,id,-6.1,-12,0,'https://roelyoon.github.io/Portfolio/Chall
 addSprite(444,300,1/50,id,5,-12,0,'https://roelyoon.github.io/Portfolio/Challenges/Challenge 2/explanation.png');
 
 //traverse
-addSprite(212,692,1/20,id,-6,-36,0,'https://roelyoon.github.io/Portfolio/Challenges/Challenge 2/traverse.png');
+addSprite(212,692,1/20,id,-6.2,-36,0,'https://roelyoon.github.io/Portfolio/Challenges/Challenge 2/traverse.png');
 
 //explanation 2
-addSprite(448,692,1/50,id,5,-26,0,'https://roelyoon.github.io/Portfolio/Challenges/Challenge 2/explanation2.png');
+addSprite(448,692,1/50,id,5,-25.8,0,'https://roelyoon.github.io/Portfolio/Challenges/Challenge 2/explanation2.png');
 
 addModel(id,5,-60,0,1/30,new THREE.Vector3(0,0.03,0),function(){},function(gltf){gltf.scene.on('click',function(ev){window.open("https://youtu.be/tBt82UaFuO0");});},'https://roelyoon.github.io/Portfolio/3DModels/youtube.glb');
 

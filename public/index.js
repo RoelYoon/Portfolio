0ec5b4c4c9e7aa05a7bff8fa90382be28488aa42
RoelYoon <roel.yoon@ggq.gg>
2023-12-01 23:35:01 +0900

    added challenge 4

--- public/index.js
+++ public/index.js
@@ -291,7 +291,7 @@ addSprite(669,373,1/45,id,6,-6.7,0,'https://roelyoon.github.io/Portfolio/Challen
 addSprite(668,286,1/46,id,6,-17.6,0,'https://roelyoon.github.io/Portfolio/Challenges/Challenge 4/s2.png');
 
 //choose sprite
-addSprite(235,118,1/6.5,id,0,-38,0,'https://roelyoon.github.io/Portfolio/Challenges/Challenge 4/choose.png');
+addSprite(235,118,1/6.5,id,0,-43,0,'https://roelyoon.github.io/Portfolio/Challenges/Challenge 4/choose.png');
 
 //pos sprite
 addSprite(183,245,1/20,id,-6.1,-31,0,'https://roelyoon.github.io/Portfolio/Challenges/Challenge 4/pos.png');

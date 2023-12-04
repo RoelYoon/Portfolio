9740b4d776a64d4fa83ea5b6e4985b8cb11baca9
RoelYoon <roel.yoon@ggq.gg>
2023-12-04 22:38:50 +0900

    Fixed default pos

--- public/index.js
+++ public/index.js
@@ -404,7 +404,7 @@ function rightArrClick(){
         curScene++;
         targetCameraPos.x+=moveX;
         targetCameraPos.y = 0; 
-        targetCameraPos.z-=id<zero?moveZ*-1:moveZ*-1;
+        targetCameraPos.z-=id<zero?moveZ:moveZ*-1;
         targetOrbitPos.x+=moveX;
         targetOrbitPos.y = 0;
         targetOrbitPos.z-=id<zero?moveZ:moveZ*-1
